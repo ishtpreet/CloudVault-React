450228daee58be98b2dfa1e83b3dc07852a12645
Ishtpreet Singh <500061526@stu.upes.ac.in>
2021-02-07 19:35:49 +0530

    API url change

--- src/Services/authService.js
+++ src/Services/authService.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 
 
-const API_URL = 'http://localhost:3300/';
-// const API_URL = 'http://192.168.29.149:3300/';
+const API_URL = 'https://api.cloudvault.ml/';
+// const API_URL = 'http://localhost:3300/';
 
 
 
